5d1b46213a00daa7a7273d5111a1dd5c3d834025
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-01-22 16:15:34 +0530

    Time: 318 ms (21.14%), Space: 11.8 MB (38.26%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    bool dp[100001];
+    
+    
+    bool winnerSquareGame(int n) {
+        
+        if(n==0){
+            return false;
+        }
+        if(dp[n]!=NULL){
+            return dp[n];
+        }
+        int winner = 0;
+        for(int i=1;i*i<=n;i++){
+            if(!winnerSquareGame(n-(i*i))){
+                winner = 1;
+                break;
+            }
+        }
+        
+        dp[n]=winner;
+        return dp[n];
+    
+        
+    }
+};
