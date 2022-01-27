6aa4405b95e389c60ab4817f714c89707e361a8e
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-01-27 17:50:21 +0530

    Time: 1847 ms (5.02%), Space: 37.7 MB (98.83%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
     int findMaximumXOR(vector<int>& nums) {
-        int ans=0,mask=0;
-        unordered_set<int>st;
-        for(int i=31;i>=0;i--){
-            mask|=(1<<i);
-            int temp=ans|(1<<i);
-            for (int j = 0; j < nums.size(); j++) {
-                int num = nums[j] & mask;
-                if (st.find(temp ^ num)!=st.end()) {
-                    ans = temp;
-                    break;
-                }
-                st.insert(num);
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.end());
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            if(ans>=pow(2,floor(log2(nums[i])+1))-1){
+                continue;
             }
-            st.clear();
+            for(int j=0;j<n;j++){
+                ans = max(nums[i]^nums[j],ans);
+            }
         }
         return ans;
+        
+        // int ans=0,mask=0;
+        // unordered_set<int>st;
+        // for(int i=31;i>=0;i--){
+        //     mask = mask | (1<<i);
+        //     int temp=ans | (1<<i);
+        //     for (int j = 0; j < nums.size(); j++) {
+        //         int num = nums[j] & mask;
+        //         if (st.find(temp ^ num)!=st.end()) {
+        //             ans = temp;
+        //             break;
+        //         }
+        //         st.insert(num);
+        //     }
+        //     st.clear();
+        // }
+        // return ans;
     }
 };
