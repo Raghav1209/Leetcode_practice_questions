55e40c84fa97636a99c0027e759b12aa430303e8
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-01-29 16:49:40 +0530

    Time: 159 ms (47.88%), Space: 63.4 MB (49.65%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,34 +1,51 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+//         int n = heights.size();
+//         int maxArea = 0;
+//         vector<int> left(n);
+//         vector<int> right(n);
+//         left[0]=-1;
+//         right[n-1]=n;
+//         int prev;
+//         for(int i=1;i<n;i++){
+//             prev = i-1;
+//             while(prev>=0 && heights[prev]>=heights[i]){
+//                 prev=left[prev];
+//             }
+//             left[i] = prev;
+//         }
+        
+//         for(int i=n-2;i>=0;i--){
+//             prev=i+1;
+//             while(prev<n && heights[prev]>=heights[i]){
+//                 prev=right[prev];
+//             }
+//             right[i]=prev;
+//         }
+        
+//         for(int i=0;i<n;i++){
+//             int width = right[i]-left[i]-1;
+//             maxArea = max(maxArea,heights[i]*width);
+//         }
+//         return maxArea;
+        
         int n = heights.size();
-        int maxArea = 0;
-        vector<int> left(n);
-        vector<int> right(n);
-        left[0]=-1;
-        right[n-1]=n;
-        int prev;
-        for(int i=1;i<n;i++){
-            prev = i-1;
-            while(prev>=0 && heights[prev]>=heights[i]){
-                prev=left[prev];
+        stack<int> st;
+        int maxArea=0;
+        for(int i=0;i<=n;i++){
+            int currHeight = i==n ? 0 : heights[i];
+            while(!st.empty() && currHeight<heights[st.top()]){
+                int top = st.top();
+                st.pop();
+                int width = st.empty() ? i : (i-st.top()-1);
+                int area = heights[top]*width;
+                maxArea = max(maxArea,area);
             }
-            left[i] = prev;
-        }
-        
-        for(int i=n-2;i>=0;i--){
-            prev=i+1;
-            while(prev<n && heights[prev]>=heights[i]){
-                prev=right[prev];
-            }
-            right[i]=prev;
-        }
-        
-        for(int i=0;i<n;i++){
-            int width = right[i]-left[i]-1;
-            maxArea = max(maxArea,heights[i]*width);
+            st.push(i);
         }
         return maxArea;
+        
     }
     
 };
