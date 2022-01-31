979483ba06d7d0eab1120247b13df680259a41d7
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-01-31 15:43:24 +0530

    Time: 40 ms (58.61%), Space: 89.8 MB (30.25%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -8,19 +8,27 @@ public:
         
         merge(nums1.begin(),nums1.end(),nums2.begin(),nums2.end(),nums3.begin());
         
-        int l = 0;
-        int h = nums3.size()-1;
-        // float y = 2;
+        // int l = 0;
+        // int h = nums3.size()-1;
         
-        while(l<=h){
-            if(l==h){
-                return nums3[l];
-            }
-            l++;
-            h--;
+        int rem = x%2;
+        int mid = (x-1)/2;
+        
+        if(rem==0){
+            return (nums3[mid]+nums3[mid+1])/2;
         }
-        double median = (nums3[l]+nums3[h])/2;
-        return median;
+        return nums3[mid];
+        
+        
+        // while(l<=h){
+        //     if(l==h){
+        //         return nums3[l];
+        //     }
+        //     l++;
+        //     h--;
+        // }
+        // double median = (nums3[l]+nums3[h])/2;
+        // return median;
         
         
     }
