b5e063310195a41e3ff6aaf0c79aa13f8111373d
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-02-01 16:02:22 +0530

    Time: 270 ms (8.27%), Space: 93.3 MB (53.45%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -2,36 +2,36 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        int maxx = 0;
-        for(int i=0;i<n-1;){
-            if(prices[i]<prices[i+1]){
-                int j = i+1;
-                while(j<n){
-                    if(prices[j]>prices[i]){
-                        int profit = prices[j]-prices[i];
-                        maxx = max(maxx,profit);
-                        j++;
-                    }else{
-                        i=j-1;
-                        break;
-                    }
-                }
-            }
-            i++;
-        }
-        return maxx;
-        // int low_price=INT_MAX;
-        // int profit = 0;
-        // int max_profit = 0;
-        // for(int i=0;i<n;i++){
-        //     if(prices[i]<low_price){
-        //         low_price=prices[i];
+        // int maxx = 0;
+        // for(int i=0;i<n-1;){
+        //     if(prices[i]<prices[i+1]){
+        //         int j = i+1;
+        //         while(j<n){
+        //             if(prices[j]>prices[i]){
+        //                 int profit = prices[j]-prices[i];
+        //                 maxx = max(maxx,profit);
+        //                 j++;
+        //             }else{
+        //                 i=j-1;
+        //                 break;
+        //             }
+        //         }
         //     }
-        //     profit = prices[i]-low_price;
-        //     if(profit>max_profit){
-        //         max_profit=profit;
-        //     }
+        //     i++;
         // }
-        // return max_profit;
+        // return maxx;
+        int low_price=INT_MAX;
+        int profit = 0;
+        int max_profit = 0;
+        for(int i=0;i<n;i++){
+            if(prices[i]<low_price){
+                low_price=prices[i];
+            }
+            profit = prices[i]-low_price;
+            if(profit>max_profit){
+                max_profit=profit;
+            }
+        }
+        return max_profit;
     }
 };
