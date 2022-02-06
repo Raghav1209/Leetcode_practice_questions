4a476b7aec5aef7bd8be1241e5c54f7571fc52e6
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-02-06 15:58:37 +0530

    Time: 7 ms (60.40%), Space: 11 MB (37.19%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,39 +1,19 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        int k = 0;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+        
+        if(nums.size()<3){
+            return nums.size();
         }
+       
+        int k = 2;
         
-        unordered_map<int,int>  :: iterator it;
-        
-        for(it=mp.begin();it!=mp.end();it++){
-            if(it->second > 2){
-                it->second=2;
-            }
-        }
-        
-        vector<int> nums2;
-        
-        // for(auto x : mp){
-        //     cout<<x.first<<"->"<<x.second<<endl;
-        // }
-        
-        for(auto x : mp){
-            while(x.second){
-                nums2.push_back(x.first);
-                x.second--;
-            }
-            
-        }
-        k = nums2.size();
-        sort(nums2.begin(),nums2.end());
-        
-        for(int i=0;i<nums2.size();i++){
-            nums[i]=nums2[i];
-        }
+       for(int i=2;i<nums.size();i++){
+           if(nums[i]!=nums[k-2]){
+               nums[k] = nums[i];
+               k++;
+           }
+       }
         
         return k;
         
