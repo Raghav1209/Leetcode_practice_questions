80c2bd843c2131c9a8b5b0835297ef91f575e19e
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-02-07 16:18:49 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (12.29%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -3,45 +3,45 @@ public:
     
  
     
-    double myPowutil(double x,int num){
+//     double myPowutil(double x,int num){
         
-        if(num==0){
-            return 1;
-        }
+//         if(num==0){
+//             return 1;
+//         }
         
-        double res = myPowutil(x,num/2);
+//         double res = myPowutil(x,num/2);
         
-        if(num%2==0){
-            return res*res;
-        }else{
-           return  x*res*res;
-        }
+//         if(num%2==0){
+//             return res*res;
+//         }else{
+//            return  x*res*res;
+//         }
 
         
         
         
     
         
-    }
+//     }
     
     double myPow(double x, int n) {
         
-        if(n==0){
-            return 1;
-        }
+//         if(n==0){
+//             return 1;
+//         }
         
-        int num = abs(n);
+//         int num = abs(n);
         
         
-        double ans = myPowutil(x,num);
+//         double ans = myPowutil(x,num);
         
-        if(n<0){
-            ans = double(1)/double(ans);
-        }
+//         if(n<0){
+//             ans = double(1)/double(ans);
+//         }
         
-        return double(ans);
+//         return double(ans);
         
-        // return pow(x,n);
+        return pow(x,n);
     
         
     }
