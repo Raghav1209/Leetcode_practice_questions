320a005455bd7cd7aa396c0a092d294df9f9218e
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-03-01 10:29:11 +0530

    Time: 12 ms (25.62%), Space: 8.4 MB (42.77%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,29 +1,52 @@
 class Solution {
 public:
+    
+    int solve(int n){
+        if(n==0){
+            return 0;
+        }
+        
+        if(n==1){
+            return 1;
+        }
+        
+        if(n%2==0){
+            return solve(n/2);
+        }else{
+            return 1+solve(n/2);
+        }
+        
+    }
+    
     vector<int> countBits(int n) {
         
         vector<int> v;
-        v.push_back(0);
+//         v.push_back(0);
         
-        if(n==0){
-            return v;
-        }
+//         if(n==0){
+//             return v;
+//         }
         
-        for(int i=1;i<=n;i++){
-            int count = 0;
-             string binary = bitset<32>(i).to_string();
-            for(int i=0;i<binary.length();i++){
-                if(binary[i]=='1'){
-                    count += 1;
-                }
+//         for(int i=1;i<=n;i++){
+//             int count = 0;
+//              string binary = bitset<32>(i).to_string();
+//             for(int i=0;i<binary.length();i++){
+//                 if(binary[i]=='1'){
+//                     count += 1;
+//                 }
                 
-            }
-             v.push_back(count);
+//             }
+//              v.push_back(count);
+//         }
+        
+        
+//         return v;
+        
+        for(int i=0;i<=n;i++){
+            int x = solve(i);
+            v.push_back(x);
         }
-        
-        
-        return v;
-           
+          return v; 
         
     }
 };
