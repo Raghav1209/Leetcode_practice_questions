b2a516be23f699c2db4a2721aea859c49fe4f4ac
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-03-10 21:54:19 +0530

    Time: 16 ms (99.44%), Space: 71.5 MB (50.05%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -0,0 +1,103 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+    ListNode* reverse(ListNode* head){
+        
+        ListNode* curr = head;
+        ListNode* nextnode = head;
+        ListNode* prev = NULL;
+        
+        while(nextnode){
+            nextnode = nextnode->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextnode;
+        }
+        
+        return prev;
+        
+    }
+    
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        if(l1==NULL){
+            return l2;
+        }
+        
+        if(l2==NULL){
+            return l1;
+        }
+        ListNode* res = NULL;
+        int carry = 0;
+        ListNode* p = l1;
+        ListNode* q = l2;
+        int sum,x;
+        while(p && q){
+            
+            if(carry){
+                sum = p->val + q->val + carry;
+            }else{
+                sum = p->val + q->val;
+            }
+             x = sum % 10;
+             carry = sum/10;
+            ListNode* node = new ListNode(x);
+            node->next = res;
+            res = node;
+            p=p->next;
+            q=q->next;
+            
+        }
+        
+        while(p){
+            if(carry){
+                sum = p->val  + carry;
+            }else{
+                sum = p->val;
+            }
+            x = sum % 10;
+            carry = sum/10;
+            ListNode* node = new ListNode(x);
+            node->next = res;
+            res = node;
+            p=p->next;
+            // q=q->next;
+        }
+        
+        while(q){
+            if(carry){
+                sum =  q->val + carry;
+            }else{
+                sum =  q->val;
+            }
+             x = sum % 10;
+             carry = sum/10;
+            ListNode* node = new ListNode(x);
+            node->next = res;
+            res = node;
+            // p=p->next;
+            q=q->next;
+        }
+        
+        if(carry){
+            ListNode* node = new ListNode(carry);
+            node->next = res;
+            res = node;
+        }
+        
+        ListNode* req = reverse(res);
+        
+        return req;
+        
+    }
+};
