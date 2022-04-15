5b7f9cd29de2232167536697a3d40a052fd802bf
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-04-15 13:56:29 +0530

    Time: 107 ms (79.95%), Space: 61.9 MB (28.61%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -12,32 +12,62 @@
 class Solution {
 public:
     
-    void deep(TreeNode* root,int level){
-        if(root==NULL){
-            return;
-        }
+//     void deep(TreeNode* root,int level){
+//         if(root==NULL){
+//             return;
+//         }
         
-        if(root->left==NULL && root->right==NULL && level>=maxlevel){
-            if(level==maxlevel){
-                sum += root->val;
-            }else{
-                sum = root->val;
-            }
+//         if(root->left==NULL && root->right==NULL && level>=maxlevel){
+//             if(level==maxlevel){
+//                 sum += root->val;
+//             }else{
+//                 sum = root->val;
+//             }
             
-            maxlevel = level;
-            return;
-        }
+//             maxlevel = level;
+//             return;
+//         }
         
-        deep(root->left,level+1);
-        deep(root->right,level+1);
+//         deep(root->left,level+1);
+//         deep(root->right,level+1);
         
-    }
+//     }
     
-    int sum = 0;
-    int maxlevel=0;
+    // int sum = 0;
+    // int maxlevel=0;
     int deepestLeavesSum(TreeNode* root) {
-        int level = 1;
-      deep(root,level);
+      //   int level = 1;
+      // deep(root,level);
+      //   return sum;
+        
+        
+        // USING QUEUE
+        
+        queue<TreeNode*> q;
+        int count;
+        q.push(root);
+        int i;
+        int sum=0;
+        while(!q.empty())
+        {
+            count=q.size();
+            sum=0;
+            for(i=0;i<count;i++)
+            {
+                TreeNode *curr=q.front();
+                q.pop();
+                if(curr->left!=NULL)
+                {
+                    q.push(curr->left);
+                    
+                }
+                if(curr->right!=NULL)
+                {
+                    q.push(curr->right); 
+                }
+                sum+=curr->val;
+            }
+        }
         return sum;
         
     }
