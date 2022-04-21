5449ca440235c09a8b926972e85472e9886c6a79
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-04-21 12:41:54 +0530

    Time: 16 ms (83.52%), Space: 5.9 MB (30.65%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        
+        if(n==1){
+            return true;
+        }
+        else if(n>1 && n%3==0){
+            return isPowerOfThree(n/3);
+        }else{
+            return false;
+        }
+        
+    }
+};
