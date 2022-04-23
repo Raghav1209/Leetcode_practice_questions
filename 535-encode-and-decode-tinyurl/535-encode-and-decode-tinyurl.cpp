04c68ba9dde33341738911884b4ca0890d051626
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-04-23 12:22:03 +0530

    Time: 4 ms (70.07%), Space: 9.3 MB (5.80%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,14 +1,45 @@
 class Solution {
 public:
-
+    map<string,string> mp;
+    const int MAX = 62;
     // Encodes a URL to a shortened URL.
+    
+ 
     string encode(string longUrl) {
-        return longUrl;
+        
+        string str = "http://tinyurl.com/";
+        
+    vector<string>alphabet{ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n",
+                          "o", "p", "q", "r", "s", "t", "u",
+                          "v", "w", "x", "y", "z","A" ,"B" ,"C", "D","E", "F", "G",
+                          "H", "I", "J", "K", "L", "M", "N",
+                          "O", "P", "Q", "R", "S", "T", "U",
+                          "V", "W", "X", "Y", "Z","0","1","2","3","4","5","6","7","8","9"};
+                         
+        
+        for(int i=0;i<6;i++){
+            str += alphabet[rand()%MAX];
+        }
+        
+        mp.insert({longUrl,str});
+        
+        return str;
+        
+        
+        
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return shortUrl;
+        
+        for(auto x : mp){
+            if(x.second==shortUrl){
+                return x.first;
+            }
+        }
+        
+        return "";
+        
     }
 };
 
