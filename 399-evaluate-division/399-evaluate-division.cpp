bd73662f1db3de67a54b3752cb15ad3a118c1b24
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-05-02 13:36:22 +0530

    Time: 3 ms (67.48%), Space: 8.4 MB (30.55%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.cpp
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -1,60 +1,76 @@
 class Solution {
-    unordered_map<string, vector<pair<string, double>>> adjList;
+    unordered_map<string, vector<pair<string, double>>> adj;
     unordered_map<string, bool> visited;
     double queryAns;
 public:
     
-    bool dfs(string startNode, string endNode, double runningProduct){
-        
-        if(startNode == endNode and adjList.find(startNode)!=adjList.end()) {
-            queryAns = runningProduct;
-            return true;
-            
+    bool findPath(string s,string d,double ans){
+        if(adj.find(s)==adj.end() || adj.find(d)==adj.end()){
+            return false;
         }
         
+        if(s==d){
+            queryAns = ans;
+            return true;
+        }
+        vector<pair<string,double>> q = adj[s];
         bool tempAns = false;
-        visited[startNode] = true;
-        
-        for(int i = 0; i < adjList[startNode].size(); i++){
-            if(!visited[adjList[startNode][i].first]){
-                tempAns = dfs(adjList[startNode][i].first, endNode, runningProduct*adjList[startNode][i].second);
+        visited[s] = true;
+    
+        for(int i=0;i<q.size();i++){
+            
+            if(!visited[q[i].first]){
+                tempAns = findPath(q[i].first,d,ans*q[i].second);
                 if(tempAns){
                     break;
                 }
             }
+            
         }
         
-        visited[startNode] = false;
+        visited[s] = false;
         
         return tempAns;
         
     }
     
+    
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
         
         
         int n = equations.size(), m = queries.size();
-        vector<double> ans(m);
+        vector<double> ans;
         
-        for(int i = 0; i < n ; i++){
+        for(int i=0;i<n;i++){
+            vector<string> pair = equations[i];
+            string source = pair[0];
+            string destination = pair[1];
             
-            adjList[equations[i][0]].push_back({equations[i][1], values[i]});
-            adjList[equations[i][1]].push_back({equations[i][0], 1/values[i]});
-            visited[equations[i][0]] = false;
-            visited[equations[i][1]] = false;
-
+            adj[source].push_back({destination,values[i]});
+            
+            adj[destination].push_back({source,1/values[i]});
+            visited[source] = false;
+            visited[destination] = false;
         }
-        
-        for(int i = 0; i < m ; i++){
-            
+        // bool temp;
+        for(int i=0;i<m;i++){
             queryAns = 1;
-            bool pathFound = dfs(queries[i][0], queries[i][1], 1);            
-            if(pathFound) ans[i] = queryAns;
-            else ans[i] = -1;
+            vector<string> query = queries[i];
+            string s = query[0];
+            string d = query[1];
+            
+            if(!visited[s]){
+                bool temp = findPath(s,d,1);
+                
+            if(temp){
+                ans.push_back(queryAns);
+            }else{
+                    ans.push_back(-1);
+                }
+            }
             
         }
         
         return ans;
-        
     }
 };
