4b9981b614a85cc9b0ac31ba68f95d6d66f2d1ec
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-06-15 13:58:41 +0530

    Time: 6 ms (21.50%), Space: 7 MB (92.48%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -14,16 +14,17 @@ public:
             i++;
             j--;
         }
-        int grid[n+1][n+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=n;j++){
-                grid[j][i] = matrix[i][j];
-            }
-        }
+        
+//         int grid[n+1][n+1];
+//         for(int i=0;i<=n;i++){
+//             for(int j=0;j<=n;j++){
+//                 grid[j][i] = matrix[i][j];
+//             }
+//         }
         
         for(int i=0;i<=n;i++){
-            for(int j=0;j<=n;j++){
-                matrix[i][j] = grid[i][j];
+            for(int j=i;j<=n;j++){
+                swap(matrix[i][j],matrix[j][i]);
             }
         }
         
