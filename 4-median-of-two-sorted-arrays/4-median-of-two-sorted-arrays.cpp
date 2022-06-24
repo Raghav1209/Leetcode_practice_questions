70160b08f370570a8eae2b6bb8d10b710e6c661e
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-06-24 16:09:46 +0530

    Time: 60 ms (48.14%), Space: 89.4 MB (61.70%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,35 +1,48 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        int x = n+m;
-        vector<float> nums3(x);
+    
+    double find(vector<int> &a1,vector<int> &a2){
+        int begin1,end1;
+        begin1 = 0;
         
-        merge(nums1.begin(),nums1.end(),nums2.begin(),nums2.end(),nums3.begin());
+        end1 = a1.size();
         
-        // int l = 0;
-        // int h = nums3.size()-1;
-        
-        int rem = x%2;
-        int mid = (x-1)/2;
-        
-        if(rem==0){
-            return (nums3[mid]+nums3[mid+1])/2;
+        while(begin1<=end1){
+            int i1 = (begin1+end1)/2;
+            int i2 = (a1.size()+a2.size()+1)/2 - i1;
+            
+            int max1 = (i1==0) ? INT_MIN : a1[i1-1];
+            int min1 = (i1==a1.size()) ? INT_MAX : a1[i1];
+            
+            int max2 = (i2==0) ? INT_MIN : a2[i2-1];
+            int min2 = (i2==a2.size()) ? INT_MAX : a2[i2];
+            
+            if(max1<=min2 && max2<=min1){
+                
+                if((a1.size()+a2.size())%2==0){
+                    return ((double) max(max1,max2) + min(min1,min2) )/2;
+                }else{
+                    return ((double) max(max1,max2));
+                }
+                
+            }else if(max1 > min2){
+                end1 = i1-1;
+            }else{
+                begin1 = i1+1;
+            }
+            
         }
-        return nums3[mid];
         
+        return (double) 0;
+    }
+    
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         
-        // while(l<=h){
-        //     if(l==h){
-        //         return nums3[l];
-        //     }
-        //     l++;
-        //     h--;
-        // }
-        // double median = (nums3[l]+nums3[h])/2;
-        // return median;
-        
+        if(nums1.size()<nums2.size()){
+            return find(nums1,nums2);
+        }else{
+            return find(nums2,nums1);
+        }
         
     }
 };
