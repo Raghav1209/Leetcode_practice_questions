e08fe95ae42bc8c3098e791254834b1670fc0426
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-06-24 14:34:42 +0530

    Time: 26 ms (92.70%), Space: 22.5 MB (8.81%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -7,15 +7,15 @@ public:
         }
         
         int low = 0;
-        int high = nums.size()-1;
+        int high = nums.size()-2;
         
         while(low<=high){
             
             int mid = (low+high)/2;
-            cout<<low<<" ";
-            cout<<mid<<" ";
-            cout<<high<<" ";
-            cout<<endl;
+            // cout<<low<<" ";
+            // cout<<mid<<" ";
+            // cout<<high<<" ";
+            // cout<<endl;
             if(mid%2==0){
                 
                 if(nums[mid]==nums[mid+1]){
