438717f4967c583c5f978949e7386e3232527cd2
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-06-29 14:07:20 +0530

    Time: 4 ms (66.86%), Space: 7.3 MB (7.75%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -1,57 +1,81 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int n = s.length();
-        int flag=0;
-        int flag2 = 0;
-        int flag3 = 0;
-        int flag4 = 0;
-        long long int num = 0;
-        for(int i=0;i<n;i++){
-            
-            if(s[i]=='+' || s[i]=='-'){
-                if(flag2 || flag3 || flag4){
-                    break;
-                }
-                if(s[i]=='-'){
-                    flag=1;
-                }
-                flag2 = 1;
-            }
-            else if(s[i]==' '){
-                if(flag4 || flag2){
-                    break;
-                }
-                continue;
-            }
-            else if((s[i]>='A' && s[i]<='Z') || (s[i]>='a' && s[i]<='z') || s[i]=='.'){
-                 break;
+        
+        int n = s.size();
+        int i = 0;
+        bool neg = false;
+        
+        while(i<n){
+            if(s[i]==' '){
+                i++;
             }else{
-                flag4 = 1;
-                int x = s[i] - '0';
-                if(x==0){
-                    flag3 = 1;
-                }
-                num = (num*10) + x;
-                // if(num < INT_MIN){
-                //     return INT_MIN;
-                // }
-                if(num>INT_MAX){
-                    if(flag){
-                        return INT_MIN;
-                    }
-                    return INT_MAX;
-                }
-                // num = num*10;
-                
+                break;
             }
+        }
+        
+        
+        if(s[i]=='-'){
+            neg = true;
+            i++;
+        }else if(s[i]=='+'){
+            i++;
+        }
+        
+        while(i<n){
+            if(s[i]!='0'){
+                break;
+            }else{
+                i++;
+            }
+        }
+        
+        string str = "";
+        long long res = 0;
+        int p = 0;
+        bool ovf = false;
+        
+        while(i<n){
+            if(s[i]>='0' && s[i]<='9'){
+                str += s[i];
+            }else{
+                break;
+            }
+            i++;
+        }
+        // cout<<str<<" ";
+        int j = str.size()-1;
+        while(j>=0){
+            
+            int val = str[j]-'0';
+            res += val*pow(10,p);
+            
+            if((p>10) || res>INT_MAX){
+                ovf = true;
+                break;
+            }
+            j--;
+            p++;
             
         }
-         // num = num/10;
-            if(flag){
-                return (num*-1);
+        
+        if(ovf){
+            if(neg){
+                return INT_MIN;
             }else{
-                return num;
+                return INT_MAX;
             }
+        }
+        
+        if(neg){
+            return res*-1;
+        }else{
+            return res;
+        }
+        
+        
+        
+        
+        
     }
 };
