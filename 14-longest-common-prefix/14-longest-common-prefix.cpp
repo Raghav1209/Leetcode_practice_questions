52b761b703a2d030f5674bbae1c0258ff6a831fb
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-06-29 14:27:48 +0530

    Time: 9 ms (33.03%), Space: 9.2 MB (79.80%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -2,25 +2,37 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         
-        sort(strs.begin(),strs.end());
+         int n = strs.size();
         
-        string str = strs[0];
+        if(n==1){
+            return strs[0];
+        }
+        sort(strs.begin(),strs.end());
+       
+        string a = strs[0];
+        string b = strs[n-1];
         string res = "";
-        for(int i=1;i<=str.size();i++){
-            int flag =1;
-            string ch = str.substr(0,i);
-            int j=i;
-            for(int i=1;i<strs.size();i++){
-                if(ch!=strs[i].substr(0,j)){
-                    flag=0;
-                }
-            }
+//         for(int i=1;i<=str.size();i++){
+//             int flag =1;
+//             string ch = str.substr(0,i);
+//             int j=i;
+//             for(int i=1;i<strs.size();i++){
+//                 if(ch!=strs[i].substr(0,j)){
+//                     flag=0;
+//                 }
+//             }
             
-            if(flag){
-                res += str[i-1];
-            }
+//             if(flag){
+//                 res += str[i-1];
+//             }
             
             
+//         }
+        
+        for(int i=0;i<b.size();i++){
+            if(a[i]==b[i]){
+                res+=a[i];
+            }else break;
         }
         
         return res;
