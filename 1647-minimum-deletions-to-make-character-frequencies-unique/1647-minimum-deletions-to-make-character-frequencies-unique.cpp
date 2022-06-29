70fb6c2235f54b193918441236e34d803a89d79e
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-06-29 15:53:26 +0530

    Time: 326 ms (5.19%), Space: 17.5 MB (37.41%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,32 +1,43 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        unordered_map<int,int>charcnt;
-        unordered_map<int,int>uniquefreq;
-        int count=0;
+        sort(s.begin(),s.end());
+        int count = 0;
+        map<char,int> mp1;
+        map<int,char> mp2;
         
-        for(int i=0;i<s.size();i++)
-        {
-            charcnt[s[i]-'a']++;
+        for(int i=0;i<s.size();i++){
+            mp1[s[i]]++;
         }
         
-        for(auto &it : charcnt)
-        {
-            int x= it.second;
-            if(uniquefreq[x]!=0)
-            {
-                while(x>0 && uniquefreq[x]!=0)
-                {
-                    x--;
-                    count++;
+        for(auto x : mp1){
+            if(mp2.find(x.second)==mp2.end()){
+                mp2[x.second] = x.first;
+            }else{
+                int y = x.second;
+                while(x.second){
+                    if(mp2.find(x.second-1)==mp2.end()){
+                        if((x.second-1)!=0)
+                        {
+                            mp2[x.second-1] = x.first;
+                        }
+                       
+                        int xx = x.second-1;
+                        count = count+(y-xx) ;
+                        break;
+                    }
+                    x.second--;
                 }
             }
-            if(x>0)
-            {
-                uniquefreq[x]++;
-            }
         }
         
+        // for(auto x : mp1){
+        //     cout<<x.first<<" "<<x.second<<endl;
+        // }
+        
         return count;
+        
+        
+        
     }
 };
