22bd1e4fd565a56603b8ab4c4054e1895590ced3
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-07-14 22:53:00 +0530

    Time: 40 ms (45.90%), Space: 26 MB (64.01%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -18,30 +18,27 @@ public:
             return NULL;
         }
         
-        TreeNode* root = new TreeNode(preorder[preIndex++]);
-        
+        TreeNode* node = new TreeNode(preorder[preIndex++]);
         int inIndex;
-        
-        for(int i=is;i<ie;i++){
-            if(inorder[i]==root->val){
+        for(int i=is;i<=ie;i++){
+            if(inorder[i]==node->val){
                 inIndex = i;
                 break;
             }
         }
         
-        root->left = solve(preorder,inorder,is,inIndex-1);
-        root->right = solve(preorder,inorder,inIndex+1,ie);
-        
-        return root;
+        node->left = solve(preorder,inorder,is,inIndex-1);
+        node->right = solve(preorder,inorder,inIndex+1,ie);
+        return node;
         
     }
     
     int preIndex = 0;
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         
-        int is = 0;
-        int ie = preorder.size()-1;
-        TreeNode* root = solve(preorder,inorder,is,ie);
-        return root;
+        int is=0;
+        int ie=preorder.size()-1;
+        return solve(preorder,inorder,is,ie);
+        
     }
 };
