881721d148a650f5f18498d4bc98ba0940f95636
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-07-17 15:56:47 +0530

    Time: 54 ms (10.65%), Space: 23.6 MB (43.62%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,32 +1,56 @@
 class Solution {
 public:
     
-    int utility(vector<vector<int>>& grid, int i, int j)
-    {
-        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size())
-            return 0;
-        if(grid[i][j]==0)
-            return 0;
-       
-        grid[i][j]=0;
-         
-            
-        return (1+ utility(grid, i+1, j) + utility(grid, i, j+1) + utility(grid, i-1, j) + utility(grid, i, j-1));
+    bool isSafe(vector<vector<int>>& grid,vector<vector<bool>> &vis,int i,int j){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || vis[i][j]==true || grid[i][j]==0){
+            return false;
+        }
+        return true;
     }
     
+    void DFS(vector<vector<int>>& grid,vector<vector<bool>> &vis,int i,int j){
+        
+        vis[i][j] = true;
+        
+        if(isSafe(grid,vis,i-1,j)){
+            count++;
+            DFS(grid,vis,i-1,j);
+        }
+        
+        if(isSafe(grid,vis,i+1,j)){
+            count++;
+            DFS(grid,vis,i+1,j);
+        }
+        
+        if(isSafe(grid,vis,i,j-1)){
+            count++;
+            DFS(grid,vis,i,j-1);
+        }
+        
+        if(isSafe(grid,vis,i,j+1)){
+            count++;
+            DFS(grid,vis,i,j+1);
+        }
+        
+    }
     
+    int count;
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        int ans = 0, x =0;
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j])
-                    x = utility(grid, i, j);
-                
-                ans = max(ans, x);
+        
+        int maxx = 0;
+        vector<vector<bool>> vis(grid.size(),vector<bool>(grid[0].size(),false));
+        
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(!vis[i][j] && grid[i][j]==1){
+                    count=1;
+                    DFS(grid,vis,i,j);
+                    maxx = max(count,maxx);
+                }
             }
-        return ans;
+        }
+        
+        return maxx;
+        
     }
 };
