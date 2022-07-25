c1558e285081cf98f7bebcde64c81d41bae5f2a7
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-07-26 00:28:30 +0530

    Time: 11 ms (68.40%), Space: 13.8 MB (17.99%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,56 +1,21 @@
 class Solution {
 public:
-    
-    int binary(vector<int> &nums,int target,int low,int high){
-        
-        while(low<=high){
-            int mid = (low+high)/2;
-            
-            if(nums[mid]==target){
-                return mid;
-            }else if(nums[mid]>target){
-                high = mid-1;
-            }else{
-                low = mid+1;
-            }
-            
-        }
-        
-        return -1;
-        
-    }
-    
     vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans;
+        vector<int> :: iterator it,lower,upper;
         
-        vector<int> ans(2);
-        ans[0] = -1;
-        ans[1] = -1;
-        int pos = binary(nums,target,0,nums.size()-1);
-        int first_occur = pos;
-        int last_occur = pos;
+        it = find(nums.begin(),nums.end(),target);
         
-        if(pos!=-1){
-            
-            if(first_occur!=0){
-                while(first_occur>0 && nums[first_occur]==nums[first_occur-1]){
-                    first_occur--;
-                }
-                ans[0] = first_occur;
-            }else{
-                ans[0] = 0;
-            }
-            
-            
-            if(last_occur!=nums.size()-1){
-                while(last_occur<nums.size()-1 && nums[last_occur]==nums[last_occur+1]){
-                    last_occur++;
-                }
-                ans[1] = last_occur;
-            }else{
-                ans[1] = nums.size()-1;
-            }
-            
-            
+        if(it!=nums.end()){
+            lower = lower_bound(nums.begin(),nums.end(),target);
+            upper = upper_bound(nums.begin(),nums.end(),target);
+            cout<<lower-nums.begin()<<endl;
+            cout<<upper-nums.begin()-1<<endl;
+            ans.push_back(lower-nums.begin());
+            ans.push_back(upper-nums.begin()-1);
+        }else{
+            ans.push_back(-1);
+            ans.push_back(-1);
         }
         
         return ans;
