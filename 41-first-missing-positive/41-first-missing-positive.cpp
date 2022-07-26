fa21ad8dca1ab230b1947508397370d85d40fcc1
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-07-26 12:22:57 +0530

    Time: 301 ms (35.05%), Space: 83.1 MB (41.35%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -2,21 +2,46 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         
-        unordered_set<int> s;
+//         unordered_set<int> s;
         
-        for(int i=0;i<nums.size();i++){
-            s.insert(nums[i]);
-        }
+//         for(int i=0;i<nums.size();i++){
+//             s.insert(nums[i]);
+//         }
     
-        int maxx = *max_element(nums.begin(),nums.end());
+//         int maxx = *max_element(nums.begin(),nums.end());
+//         int i;
+//         for(i=1;i<=maxx;i++){
+//             if(s.find(i)==s.end()){
+//                 return i;
+//             }
+//         }
+        
+//         return i;
+        
+        int n = nums.size();
         int i;
-        for(i=1;i<=maxx;i++){
-            if(s.find(i)==s.end()){
-                return i;
+        for(i=0;i<n;i++){
+            
+            if(nums[i]<=0 || nums[i]>n){
+                continue;
+            }
+            
+            if(nums[nums[i]-1]==nums[i]){
+                continue;
+            }else{
+                swap(nums[i],nums[nums[i]-1]);
+                i--;
+            }
+            
+        }
+        
+        for(i=0;i<n;i++){
+            if(i+1!=nums[i]){
+                return i+1;
             }
         }
         
-        return i;
+        return n+1;
         
     }
 };
