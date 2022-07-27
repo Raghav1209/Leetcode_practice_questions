076f17a999d2021b7cb7c8737c26aeb9749acb8f
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-07-27 11:24:12 +0530

    Time: 8 ms (67.50%), Space: 12.8 MB (38.74%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -13,15 +13,20 @@ class Solution {
 public:
     
     void flatten(TreeNode* root) {
+        
         if(root==NULL){
             return;
         }
+        
         stack<TreeNode*> st;
         st.push(root);
         
         while(!st.empty()){
+            
             TreeNode* curr = st.top();
             st.pop();
+            
+            
             
             if(curr->right){
                 st.push(curr->right);
@@ -31,10 +36,13 @@ public:
                 st.push(curr->left);
             }
             
-            if(!st.empty()){
-                curr->right = st.top();
-            }
             curr->left = NULL;
+            
+            if(!st.empty()){
+                 curr->right = st.top();
+            }
+           
+            
         }
         
     }
