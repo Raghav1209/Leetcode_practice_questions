4f0512fd0f76f8999787fe62790ced1060d81be7
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-07-29 00:18:39 +0530

    Time: 3 ms (45.91%), Space: 6.5 MB (57.43%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,66 +1,42 @@
 class Solution {
 public:
     
-    vector<string> merge(vector<string> a,vector<string> b){
-        vector<string> req;
-        for(int i=0;i<a.size();i++){
-            for(int j=0;j<b.size();j++){
-                string str = "";
-                str += a[i];
-                str += b[j];
-                req.push_back(str);
-            }
+    unordered_map<char,string> mp;
+    vector<string> ans;
+    
+    void solve(string digits,string str,int i,unordered_map<char,string> &mp){
+        
+        if(i==digits.size()){
+            ans.push_back(str);
+            return;
         }
-        return req;
+        
+        for(auto letters : mp[digits[i]]){
+            str.push_back(letters);
+            solve(digits,str,i+1,mp);
+            str.pop_back();
+        }
+        
     }
     
     vector<string> letterCombinations(string digits) {
-    
-        int n = digits.length();
         
-          if(n==0){
-              vector<string> ans;
+        if(digits.size()==0){
             return ans;
         }
-    
-        map<char,vector<string>> m;
-        m.insert({'2',{"a","b","c"}});
-         m.insert({'3',{"d","e","f"}});
-         m.insert({'4',{"g","h","i"}});
-         m.insert({'5',{"j","k","l"}});
-         m.insert({'6',{"m","n","o"}});
-         m.insert({'7',{"p","q","r","s"}});
-         m.insert({'8',{"t","u","v"}});
-         m.insert({'9',{"w","x","y","z"}});
         
-        // if(n==1){
-        //     return m[digits[0]].second;
-        // }else if(n==2){
-        //     return merge(m[digits[0]].second,m[digits[1]].second);
-        // }else if(n==3){
-        //     vector<string> v = merge(m[digits[0]].second,m[digits[1]].second);
-        //     return merge(v,m[2].second);
-        // }else{
-        //     vector<string> v = merge(m[digits[0]].second,m[digits[1]].second);
-        //     vector<string> v1 = merge(m[digits[2]].second,m[digits[3]].second);
-        //     return merge(v,v1);
-        // }
+        mp['2'] = "abc";
+        mp['3'] = "def";
+        mp['4'] = "ghi";
+        mp['5'] = "jkl";
+        mp['6'] = "mno";
+        mp['7'] = "pqrs";
+        mp['8'] = "tuv";
+        mp['9'] = "wxyz";
         
-        if(n==1){
-            return m[digits[0]];
-        }else if(n==2){
-            return merge(m[digits[0]],m[digits[1]]);
-        }else if(n==3){
-            vector<string> v = merge(m[digits[0]],m[digits[1]]);
-            return merge(v,m[digits[2]]);
-        }else{
-            vector<string> v = merge(m[digits[0]],m[digits[1]]);
-            vector<string> v1 = merge(m[digits[2]],m[digits[3]]);
-            return merge(v,v1);
-        }
+        string str = "";
+        solve(digits,str,0,mp);
+        return ans;
         
-        
-        
-            
     }
 };
