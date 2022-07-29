cf867793283da9e5bf0a16f2911c2a35090d6930
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-07-29 13:08:05 +0530

    Time: 4 ms (76.53%), Space: 8.7 MB (29.81%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,48 +1,51 @@
 class Solution {
 public:
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
-        map<char,int> p;
-        vector<string> ans;
-        bool is_right;
         
-        for(int i=0;i<pattern.length();i++)
-            p[pattern[i]]++;
-        for(int i=0;i<words.size();i++)
-        {
-            map<char,char> mp;
-            map<char,int> w;
-            is_right = true;
-            string word = words[i];
-            for(int j=0;j<word.length();j++)
-            {
-                w[word[j]]++;
-            }
-            if(w.size() == p.size()){
-                for(int j=0;j<p.size();j++){
-                    if(p[pattern[j]] != w[word[j]])
-                        continue;
+        vector<string> ans;
+        
+    
+        int flag;
+        for(string w : words){
+            unordered_map<char,char> word;
+            unordered_map<char,char> pat;
+            flag=0;
+            if(w.size()==pattern.size()){
+                for(int i=0;i<pattern.size();i++){
+                    if(pat.find(pattern[i])==pat.end() && word.find(w[i])==word.end()){
+                        pat[pattern[i]] = w[i];
+                        word[w[i]] = pattern[i];
+                    }else{
+                        
+                        if(pat.find(pattern[i])!=pat.end() && word.find(w[i])!=word.end()){
+                            
+                            if(pat[pattern[i]]!=w[i]){
+                                flag=1;
+                                break;
+                            }
+                            
+                        }else{
+                            flag=1;
+                            break;
+                        }
+                        
+                    }
                 }
+                
+                if(flag==0){
+                    ans.push_back(w);
+                }
+                
             }else{
                 continue;
             }
-            for(int j=0;j<word.length();j++)
-            {
-                if(mp.find(pattern[j]) == mp.end())
-                    mp[pattern[j]] = word[j];
-                else{
-                    if(mp[pattern[j]] == word[j])
-                        is_right = true;
-                    else
-                    {
-                        is_right = false;
-                        break;
-                    }   
-                }
-            }
-            if(is_right)
-                ans.push_back(word);
+            
+            
+            
         }
+        
         return ans;
+        
         
     }
 };
