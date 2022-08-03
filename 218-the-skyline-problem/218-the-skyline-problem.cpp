4c6e35f8be12407163b063440c3291dcdd67f354
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-08-03 12:48:05 +0530

    Time: 29 ms (95.66%), Space: 15.6 MB (42.97%) - LeetHub

---
+++ 218-the-skyline-problem/218-the-skyline-problem.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    
+//     static bool mycmp(pair<int,int> &a,pair<int,int> &b){
+        
+//         if(a.first==b.first){
+//             return a.second<b.second;
+//         }
+        
+//         return a.first<a.second;
+        
+//     }
+    
+    vector<vector<int>> getSkyline(vector<vector<int>>& b) {
+        
+        vector<pair<int,int>> v;
+        vector<vector<int>> ans;
+        
+        for(int i=0;i<b.size();i++){
+            vector<int> vc = b[i];
+            v.push_back({vc[0],-1*vc[2]});
+            v.push_back({vc[1],vc[2]});
+        }
+        
+        
+        
+        multiset<int,greater<int>> pq;
+        pq.insert(0);
+        
+        int curr_ht = 0;
+        
+        sort(v.begin(),v.end());
+        
+        for(auto x : v){
+            
+            int co = x.first;
+            int ht = x.second;
+            
+            if(ht<0){
+                pq.insert(-ht);
+            }else{
+               pq.erase(pq.find(ht));
+            }
+            
+            if(curr_ht!=*pq.begin()){
+                vector<int> a;
+                a.push_back(co);
+                a.push_back(*pq.begin());
+                curr_ht = *pq.begin();
+                ans.push_back(a);
+            }
+            
+        }
+        
+        return ans;
+        
+    }
+};
