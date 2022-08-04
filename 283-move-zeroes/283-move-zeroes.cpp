436d4f62e941668f6be63f53044629436fa46b81
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-08-04 13:32:36 +0530

    Time: 39 ms (48.68%), Space: 19.3 MB (57.19%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -2,21 +2,38 @@ class Solution {
 public:
     
     void moveZeroes(vector<int>& nums) {
-        int i;
-        for(i=0;i<nums.size();i++)
-        {
-            if(nums[i] == 0)
-            {
-                int j = i+1;
-                while(j<nums.size() && nums[j]==0){
-                    j++;
-                }
-                if(j<nums.size()){
-                    swap(nums[i],nums[j]);
-                }else{
-                    break;
-                }
+        // int i;
+        // for(i=0;i<nums.size();i++)
+        // {
+        //     if(nums[i] == 0)
+        //     {
+        //         int j = i+1;
+        //         while(j<nums.size() && nums[j]==0){
+        //             j++;
+        //         }
+        //         if(j<nums.size()){
+        //             swap(nums[i],nums[j]);
+        //         }else{
+        //             break;
+        //         }
+        //     }
+        // }
+        
+        int i = 0;
+        int j = 1;
+        
+        while(j<nums.size()){
+            if(nums[i]==0 && nums[j]!=0){
+                swap(nums[i],nums[j]);
+                i++;
+                j++;
+            }else if(nums[i]==0 && nums[i]==0){
+                j++;
+            }else{
+                i++;
+                j++;
             }
         }
+        
     }
 };
