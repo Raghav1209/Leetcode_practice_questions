bbf128013fabb81d208781e5e9fd8f4059e1d106
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-08-08 11:55:56 +0530

    Time: 15 ms (83.26%), Space: 10.5 MB (69.86%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,47 +1,56 @@
 class Solution {
 public:
     
-//     int find(vector<int> &tail,int l,int h,int x){
-//         if(l<h){
-//             int mid = (l+h)/2;
-//             if(tail[mid]>=x){
-//                 h = mid;
-//             }else{
-//                 l = mid+1;
-//             }
-//         }
-//         return l;
-//     }
+    // int find(int tail[],int l,int h,int x){
+    //     if(l<h){
+    //         int mid = (l+h)/2;
+    //         if(tail[mid]>=x){
+    //             h = mid;
+    //         }else{
+    //             l = mid+1;
+    //         }
+    //     }
+    //     return l;
+    // }
     
     int lengthOfLIS(vector<int>& nums) {
         
         int n = nums.size();
-//         vector<int> tail;
-//         tail.push_back(nums[0]);
+        vector<int> tail;
+        // int tail[n];
+        // int len = 0;
         
-//         for(int i=1;i<n;i++){
-//             if(nums[i]>=tail.back()){
-//                 tail.push_back(nums[i]);
-//             }
-//             else{
-//                 int c = find(tail,0,tail.size()-1,nums[i]);
-//                 tail[c] = nums[i];
-//             }
-//         }
-        
-//         return tail.size();
-        
-        vector<int> lis(n);
-        lis[0] = 1;
+        // tail[len] = nums[0];
+        // len++;
+        tail.push_back(nums[0]);
         for(int i=1;i<n;i++){
-            lis[i] = 1;
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    lis[i] = max(lis[j]+1,lis[i]);
-                }
+            if(nums[i]>tail.back()){
+                tail.push_back(nums[i]);
+                // len++;
+            }
+            else{
+                int c = lower_bound(tail.begin(),tail.end(),nums[i]) - tail.begin();
+                tail[c] = nums[i];
             }
         }
-        int x = *max_element(lis.begin(),lis.end());
-        return x;
+        
+        // for(int x=0;x<len;x++){
+        //     cout<<tail[x]<<" ";
+        // }
+        
+        return tail.size();
+        
+        // vector<int> lis(n);
+        // lis[0] = 1;
+        // for(int i=1;i<n;i++){
+        //     lis[i] = 1;
+        //     for(int j=0;j<i;j++){
+        //         if(nums[j]<nums[i]){
+        //             lis[i] = max(lis[j]+1,lis[i]);
+        //         }
+        //     }
+        // }
+        // int x = *max_element(lis.begin(),lis.end());
+        // return x;
     }
 };
