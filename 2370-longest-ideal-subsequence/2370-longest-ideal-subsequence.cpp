c0e20281b6e5a3e8f64a4cf46602ec4dccd0568d
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-08-11 16:28:10 +0530

    Time: 1130 ms (28.57%), Space: 477.5 MB (42.86%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -11,19 +11,19 @@ public:
             return dp[idx][prev];
         }
         
-        int not_take=0;
+        int maxx = -1;
         int take = 0;
         
-        not_take = solve(s,k,prev,idx+1,dp);
+        int not_take = solve(s,k,prev,idx+1,dp);
     
         
         if(prev==0 || abs(s[idx]-prev)<=k){
             take = 1+solve(s,k,s[idx],idx+1,dp);
         }
         
-        dp[idx][prev] = max(take,not_take);
+        maxx = max(take,not_take);
         
-        return dp[idx][prev];
+        return dp[idx][prev] = maxx;
         
     }
     
@@ -31,8 +31,6 @@ public:
         
         int prev = 0;
         vector<vector<int>> dp(s.size()+1,vector<int>(150,-1));
-        
-        
         
         return solve(s,k,prev,0,dp);
     }
