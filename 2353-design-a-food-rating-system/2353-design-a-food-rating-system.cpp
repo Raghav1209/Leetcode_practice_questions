1f2643507848ee744396159b6bbd881da80c83df
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-08-13 13:59:06 +0530

    Time: 807 ms (68.27%), Space: 166 MB (48.98%) - LeetHub

---
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
@@ -0,0 +1,44 @@
+class FoodRatings {
+public:
+    
+    typedef pair<int,string> pi;
+    unordered_map<string,set<pi>> mp;
+    unordered_map<string,string> cs;
+    unordered_map<string,int> rt;
+    
+    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
+        int n = foods.size();
+        for(int i=0;i<n;i++){
+            mp[cuisines[i]].insert({-ratings[i],foods[i]});
+            cs[foods[i]] = cuisines[i];
+            rt[foods[i]] = -ratings[i];
+        }
+        
+    }
+    
+    void changeRating(string food, int newRating) {
+        
+        string c = cs[food];
+        int r = rt[food];
+        
+        mp[c].erase({r,food});
+        mp[c].insert({-newRating,food});
+        rt[food] = -newRating;
+        
+    }
+    
+    string highestRated(string cuisine) {
+        
+        auto p = begin(mp[cuisine]);
+        
+        return p->second;
+        
+    }
+};
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings* obj = new FoodRatings(foods, cuisines, ratings);
+ * obj->changeRating(food,newRating);
+ * string param_2 = obj->highestRated(cuisine);
+ */
