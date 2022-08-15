dfc965378c552501cf678a26f14ddf308994d04f
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-08-15 11:54:31 +0530

    Time: 19 ms (50.83%), Space: 5.9 MB (97.90%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,83 +1,89 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int res = 0;
+        
         int first,second;
+        int ans = 0;
+        
         for(int i=0;i<s.size();i++){
             
             switch(s[i]){
-                case 'M' :
+                
+                case 'M':
                     first = 1000;
                     break;
-                    
-                case 'D':
+
+                 case 'D':
                     first = 500;
                     break;
-                
-                case 'C':
+
+                 case 'C':
                     first = 100;
                     break;
-                    
-                case 'L' : 
+
+                 case 'L':
                     first = 50;
                     break;
-                
-                case 'X':
+
+                 case 'X':
                     first = 10;
                     break;
-                
+
                 case 'V':
                     first = 5;
                     break;
-                
-                case 'I' :
+
+                case 'I':
                     first = 1;
                     break;
+                
             }
             
-            if(i!=s.size()-1){
+            if(i+1!=s.size()){
                 switch(s[i+1]){
-                case 'M' :
-                    second = 1000;
-                    break;
-                    
-                case 'D':
-                    second = 500;
-                    break;
-                
-                case 'C':
-                    second = 100;
-                    break;
-                    
-                case 'L' : 
-                    second = 50;
-                    break;
-                
-                case 'X':
-                    second = 10;
-                    break;
-                
-                case 'V':
-                    second = 5;
-                    break;
-                
-                case 'I' :
-                    second = 1;
-                    break;
-            }
-                if(first<second){
-                    res -= first;
-                }else{
-                    res += first;
+                    case 'M':
+                        second = 1000;
+                        break;
+
+                     case 'D':
+                        second = 500;
+                        break;
+
+                     case 'C':
+                        second = 100;
+                        break;
+
+                     case 'L':
+                        second = 50;
+                        break;
+
+                     case 'X':
+                        second = 10;
+                        break;
+
+                    case 'V':
+                        second = 5;
+                        break;
+
+                    case 'I':
+                        second = 1;
+                        break;
                 }
             }else{
-                res += first;
+                 ans += first;
+                break;
             }
             
+            if(first>=second){
+                ans += first;
+            }else{
+                ans -= first;
+            }
             
             
         }
-        return res;
+        
+        return ans;
         
     }
 };
