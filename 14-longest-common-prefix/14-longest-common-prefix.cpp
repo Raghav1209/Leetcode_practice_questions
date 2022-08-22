0c7981929e0e69c6f426cdbfce6611a43669c10d
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-08-22 14:19:46 +0530

    Time: 4 ms (79.29%), Space: 9.2 MB (56.06%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -12,27 +12,12 @@ public:
         string a = strs[0];
         string b = strs[n-1];
         string res = "";
-//         for(int i=1;i<=str.size();i++){
-//             int flag =1;
-//             string ch = str.substr(0,i);
-//             int j=i;
-//             for(int i=1;i<strs.size();i++){
-//                 if(ch!=strs[i].substr(0,j)){
-//                     flag=0;
-//                 }
-//             }
-            
-//             if(flag){
-//                 res += str[i-1];
-//             }
-            
-            
-//         }
         
         for(int i=0;i<b.size();i++){
             if(a[i]==b[i]){
                 res+=a[i];
-            }else break;
+            }else 
+                break;
         }
         
         return res;
