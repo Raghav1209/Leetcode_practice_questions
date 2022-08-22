d35957fa6812381a75126fa0740aa431e43543c3
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-08-22 23:19:40 +0530

    Time: 13 ms (7.99%), Space: 16.9 MB (8.56%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
     
-    vector<vector<int>> ans;
-    
-    // solve function for generating answer
-    void solve(int i, vector<int>&arr, vector<int>&temp)
-    {
-        if(i == arr.size()) // if index reaches the size of the array 
-        {
-            //if we find out a possibility, push it into the answer array
-            ans.push_back(temp);
+    void solve(vector<int> arr,int n,vector<int> v,vector<vector<int>> &ans){
+        
+        if(n==0){
+            ans.push_back(v);
             return;
         }
         
-        // taking ith element
-        temp.push_back(arr[i]);
-        
-        // call for the next index
-        solve(i + 1, arr, temp);
-        
-        // not taking ith ele,so first pop that already pushed ele, and then call for next index and generating the next possibility
-        temp.pop_back();
-        
-        // calling for next index
-        solve(i + 1, arr, temp);
+        solve(arr,n-1,v,ans);
+        v.push_back(arr[n-1]);
+        solve(arr,n-1,v,ans);
         
     }
     
     vector<vector<int>> subsets(vector<int>& arr) {
-         ans.clear(); // clear global array
-        
-        vector<int> temp; // temporary array for generating answer
-        solve(0, arr, temp); // generating all possibility
-        
+        int n = arr.size();
+        vector<vector<int>> ans;
+        vector<int> v;
+        solve(arr,n,v,ans);
         return ans;
+        
     }
 };
