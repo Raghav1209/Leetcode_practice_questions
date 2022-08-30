3a24befe802c79728fa68b71134ab210b1e3b3cc
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-08-31 01:50:20 +0530

    Time: 16 ms (30.72%), Space: 13 MB (79.46%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     
     int dx[4] = {-1,1,0,0};
-    int dy[4] = {0,0,1,-1};
+    int dy[4] = {0,0,-1,1};
     
     bool isValid(int idx,int lim){
         if(idx>=0 && idx<lim){
@@ -11,66 +11,73 @@ public:
             return false;
         }
     }
+    
     int orangesRotting(vector<vector<int>>& grid) {
         
+        int n = grid.size();
+        int m = grid[0].size();
         int total = 0;
-        int time=0;
-        int count=0;
         queue<pair<int,int>> q;
+        // vector<vector<bool>> vis(n,vector<bool>(m,false));
+        int time = 0;
+        int count = 0;
         
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==1 || grid[i][j]==2){
-                    total++;
                     if(grid[i][j]==2){
-                         q.push({i,j});
+                        q.push({i,j});
                     }
-                   
-                    
+                    total++;
                 }
             }
         }
         
-        
         while(!q.empty()){
+            int sz = q.size();
             
-            int k = q.size();
-            count += k;
+            count += sz;
             
-            while(k--){
+            // cout<<count<<" ";
+            
+            for(int i=0;i<sz;i++){
                 int x = q.front().first;
                 int y = q.front().second;
                 q.pop();
+                
                 for(int k=0;k<4;k++){
-                    int i = x+dx[k];
-                    int j = y+dy[k];
-
-                    if(isValid(i,grid.size()) && isValid(j,grid[0].size())){
-                        if(grid[i][j]==1){
-                            grid[i][j] = 2;
-                            q.push({i,j});
+                    int ii = x + dx[k];
+                    int jj = y + dy[k];
+                    
+                    if(isValid(ii,n) && isValid(jj,m)){
+                        if(grid[ii][jj]==1){
+                            grid[ii][jj] = 2;
+                            q.push({ii,jj});
                         }
                     }
-
+                    
                 }
+                
+
             }
             
-            
-            if(!q.empty()){
+            // if(!q.empty()){
+            //     time++;
+            // }
+              
+            if(count!=total){
                 time++;
             }
-            
-            
-            
-            
+                
             
         }
         
-        if(total==count){
+        if(count==total){
             return time;
         }else{
             return -1;
         }
+        
         
     }
 };
