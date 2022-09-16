f57117b8ef01c650341abd9bade84b5d61a8d73d
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-09-17 00:20:39 +0530

    Time: 137 ms (18.89%), Space: 42.4 MB (46.56%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-    int maxScore(vector<int>& cardPoints, int k) {
+    int maxScore(vector<int>& c, int k) {
         
-        int n = cardPoints.size();
-        int total = 0;
-        int sum = 0;
+        int n = c.size();
+        
         int window = n-k;
-        int ans;
+        int sum = 0;
+        int sum1 = 0;
+        int min_sum = INT_MAX;
         
-        for(int i=0;i<n;i++){
-            total += cardPoints[i];
+        for(int i=0;i<c.size();i++){
+            sum1 += c[i];
         }
         
         for(int i=0;i<window;i++){
-            sum += cardPoints[i];
-        }
-        ans = sum;
-        
-        for(int i=0;i<n-window;i++){
-            
-            sum -= cardPoints[i];
-            sum += cardPoints[i+window];
-            ans = min(ans,sum);
-            
+            sum += c[i];
         }
         
-        return total-ans;
+        min_sum = min(sum,min_sum);
+        
+        for(int i=1;i<=n-window;i++){
+            
+            sum += c[i+window-1];
+            sum -= c[i-1];
+            
+            min_sum = min(sum,min_sum);
+            
+        }
+        
+        return sum1-min_sum;
+        
+        
+        
     }
-        
-    
 };
