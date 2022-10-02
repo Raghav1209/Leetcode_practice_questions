971a632b65991d1d14ae2c06960b9d2fb9302570
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-10-02 22:32:00 +0530

    Time: 72 ms (30.98%), Space: 8.5 MB (45.97%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -11,12 +11,22 @@ public:
             return 0;
         }
         
+        // if(d==0){
+        //     if(t==0){
+        //         return 1;
+        //     }
+        //     return 0;
+        // }
+        
         if(dp[d][t]!=-1){
             return dp[d][t];
         }
         
         int count  = 0;
         for(int i=1;i<=f;i++){
+            // if(i>t){
+            //     break;
+            // }
             count = ((count%mod) + (solve(d-1,f,t-i,dp)%mod))%mod;
         }
         
@@ -26,7 +36,7 @@ public:
     
     int numRollsToTarget(int d, int f, int t) {
         
-        vector<vector<int>> dp(32,vector<int>(1002,-1));
+        vector<vector<int>> dp(d+1,vector<int>(t+1,-1));
         return solve(d,f,t,dp)%mod;
         
     }
