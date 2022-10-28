4573d7d60607033fd5fc3bcd396f586700f6f9c2
RAGHAV GHAI <87064439+Raghav1209@users.noreply.github.com>
2022-10-28 17:54:23 +0530

    Time: 251 ms (9.11%), Space: 68 MB (5.03%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    
+
+    map<vector<int>,vector<string>> mp;
+    vector<vector<string>> ans;
+    // vector<int> v;
+
+    for(int i=0;i<strs.size();i++){
+        string str = strs[i];
+        vector<int> count(256,0);
+        for(int i=0;i<str.size();i++){
+            count[str[i]-'0']++;
+        }
+
+        if(mp.find(count)==mp.end()){
+            mp[count].push_back(strs[i]);
+        }else{
+            mp[count].push_back(strs[i]);
+        }
+        
+        // mp[count].push_back(strs[i]);
+
+    }
+
+    // unordered_map<vector<int>,vector<string>> :: iterator it;
+
+    for(auto &x : mp){
+        vector<string> res = x.second;
+        ans.push_back(res);
+    }
+
+    return ans;
+}
+    
+
+
+
+
+// strs = ["eat","tea","tan","ate","nat","bat"]
+// [eat, tea, ate],  [tan, nat], [bat]
+
+// O(n * klogk)
+};
